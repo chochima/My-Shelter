f40bc60ed1df2d9d3556fd15be90a0dbdc9c5074
chong <c22425688@gmail.com>
2025-08-26 22:03:20 +0800

    re

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/My-Shelter/',
+  base: '/chochima/My-Shelter/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
