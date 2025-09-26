42e14e65050085ed45de6346b0609ced0d1e7c8e
xuu <chochimalupa@gmail.com>
2025-09-27 00:37:30 +0800

    上傳首頁內容v2

--- main.js
+++ main.js
@@ -4,32 +4,3 @@ import './assets/js/swiper-init.js';
 console.log('Hello world');
 
 
-
-//輪播套件
-// 引入 Swiper (含所有模組)
-import Swiper from 'swiper/bundle';
-// 引入樣式
-import 'swiper/css/bundle';
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: false,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
