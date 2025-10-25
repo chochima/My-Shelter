8e52b2e3d6c5c9c12c13e945c0bbb992ee1b85e3
xuu <chochimalupa@gmail.com>
2025-10-26 01:32:19 +0800

    swiper修復

--- assets/js/swiper-init.js
+++ assets/js/swiper-init.js
@@ -84,7 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     on: {
     init: function() {
-      //AOS.refresh(); // Swiper 準備好後 refresh AOS
+      AOS.refresh(); // Swiper 準備好後 refresh AOS
     }
   }
   });
@@ -120,7 +120,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     on: {
     init: function() {
-      //AOS.refresh(); // Swiper 準備好後 refresh AOS
+      AOS.refresh(); // Swiper 準備好後 refresh AOS
     }
   }
   });
@@ -164,6 +164,7 @@ document.addEventListener("DOMContentLoaded", () => {
       },   
     },
 
+    
     
   });
 
